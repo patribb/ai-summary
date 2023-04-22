c1a0fdd750a4517e3d7fe929550cb692b8883b72
Patricia Garcia <martinez171214@gmail.com>
2023-04-22 11:48:43 +0200

    initial deploy

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Demo, Hero } from "./components"
+
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  )
+}
+export default App
